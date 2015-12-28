3a6a38b35fadcdc7abd2eb719bd6845f5b424a02
mydongistiny <jaysonedson@gmail.com>
2015-12-28 14:20:25 -0800

    MetricsLogger: Rename all to DONT_TRACK_ME_BRO
    Change-Id: Ib03f3b6845bd99fd71ad6434fa2a18b7ee8ea3a7

--- src/com/android/settings/benzo/animation/AnimationSettings.java
+++ src/com/android/settings/benzo/animation/AnimationSettings.java
@@ -52,7 +52,7 @@ public class AnimationSettings extends SettingsPreferenceFragment
 
     @Override
     protected int getMetricsCategory() {
-        return MetricsLogger.DEVELOPMENT;
+        return MetricsLogger.DONT_TRACK_ME_BRO;
     }
 
     @Override
