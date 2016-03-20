1f21e4a7ce3492f8c99807df442a09d8ca733f45
Shilin Victor <chrono.monochrome@gmail.com>
2016-03-20 18:04:16 +0700

    Revert "Revert "apn: Update the add actionbar icon""
    This reverts commit e8433866b607c45869fe2eacc0a6ef5c53c3987a.
    
    Change-Id: I35bb3ee5e7600d119e27d93d95a27146cb9d484e

--- src/com/android/settings/ApnSettings.java
+++ src/com/android/settings/ApnSettings.java
@@ -343,10 +343,12 @@ public class ApnSettings extends SettingsPreferenceFragment implements
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         if (!mUnavailable) {
-            menu.add(0, MENU_NEW, 0,
-                    getResources().getString(R.string.menu_new))
-                    .setIcon(android.R.drawable.ic_menu_add)
-                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+            if (mAllowAddingApns) {
+                menu.add(0, MENU_NEW, 0,
+                        getResources().getString(R.string.menu_new))
+                        .setIcon(R.drawable.ic_menu_add)
+                        .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+            }
             menu.add(0, MENU_RESTORE, 0,
                     getResources().getString(R.string.menu_restore))
                     .setIcon(android.R.drawable.ic_menu_upload);
