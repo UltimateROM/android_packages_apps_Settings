mintkat/packages_apps_Settings

Benzo Extra Info
- This adds the ability to give users extra info in the About Phone/Tablet section
- This also does not require us to clutter up the build.prop
- The information provided to the user will be pulled from several places automatically
- Removed hardware info patch by roman/aokp

http://i.imgur.com/ywBeqDj.png
http://i.imgur.com/G4pH0wl.png
http://i.imgur.com/usVGrCy.png

Credit/Thanks to Chris_84 for his guide explaining build.prop values
http://forum.xda-developers.com/galaxy-s3/general/guide-build-prop-wiki-t2047076

**DOES NOT REQUIRE TO PLACE ANY OVERLAYS**

If you have any from the roman/aokp patch, please remove after merging

Change-Id: Ia22735508935d675ed5a9deca17953a9de03f23e

Conflicts:
	AndroidManifest.xml

src/com/android/settings/xd/extrainfo/ExtraInfoLib.java
@@ -1,0 +1,156 @@
+/*
+ * Copyright (C) 2014 The Dirty Unicorns project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.xd.extrainfo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import com.android.settings.R;
+
+public final class ExtraInfoLib {
+        private ExtraInfoLib() {
+                throw new AssertionError();
+        }
+
+        public static int dpToPx(Context context, int dp) {
+            float scale = context.getResources().getDisplayMetrics().density;
+            return (int) (dp * scale + 0.5f);
+        }
+
+    public static String ucFirst(String str) {
+        if (str == null) throw new NullPointerException("null");
+        if (str.length() == 0) return str;
+        char[] chars = str.toCharArray();
+        chars[0] = Character.toUpperCase(chars[0]);
+        return new String(chars);
+    }
+
+    public static String ucWords(String str) {
+        if (str == null) throw new NullPointerException("null");
+        if (str.length() == 0) return str;
+        final StringBuilder result = new StringBuilder(str.length());
+        String[] words = str.split("\\s");
+        for(int i = 0; i < words.length; ++i) {
+            if(i>0) result.append(" ");
+            result.append(ucFirst(words[i]));
+        }
+        return result.toString();
+    }
+
+    public static List<String> shellExec(String command)
+                throws IOException, SecurityException {
+        List<String> list = new ArrayList<String>();
+        BufferedReader stdInput = null;
+        try {
+            Process p = Runtime.getRuntime().exec(command);
+            stdInput = new BufferedReader(new
+                InputStreamReader(p.getInputStream()));
+            String s;
+            while ((s = stdInput.readLine()) != null) list.add(s);
+        } catch (IOException e) {
+                stdInput.close();
+                throw e;
+        }
+        catch (SecurityException e) {
+                stdInput.close();
+                throw e;
+        }
+        stdInput.close();
+
+        return list;
+    }
+
+    public static String round(double number, int decimalPlaces) {
+            return round(number, decimalPlaces, false);
+    }
+    public static String round(double number, int decimalPlaces, boolean groupDigits) {
+        NumberFormat numFormat = NumberFormat.getInstance();
+        numFormat.setMaximumFractionDigits(decimalPlaces);
+        numFormat.setGroupingUsed(groupDigits);
+        return numFormat.format(number);
+    }
+
+    public static double scaleData(double value, String oldScale, String newScale) {
+        final double FAILED = -1;
+        if (oldScale.length() > 2 || newScale.length() > 2) return FAILED;
+        if (oldScale.equals(newScale)) return value;
+
+        int oldMultiplier = 0;
+        int oldBase = 0;
+        int newMultiplier = 0;
+        int newBase = 0;
+
+        if (oldScale.length() == 2) {
+            if (oldScale.startsWith("k")) oldMultiplier = 1024;
+            else if (oldScale.startsWith("K")) oldMultiplier = 1000;
+            else if (oldScale.startsWith("m")) oldMultiplier = 1024 * 1024;
+            else if (oldScale.startsWith("M")) oldMultiplier = 1000 * 1000;
+            else if (oldScale.startsWith("g")) oldMultiplier = 1024 * 1024 * 1024;
+            else if (oldScale.startsWith("G")) oldMultiplier = 1000 * 1000 * 1000;
+            else return FAILED;
+
+            if (oldScale.endsWith("b")) oldBase = 1;
+            else if (oldScale.endsWith("B")) oldBase = 8;
+            else return FAILED;
+        }
+        else if (oldScale.length() == 1) {
+            if (oldScale.startsWith("k")) { oldMultiplier = 1024; oldBase = 1; }
+            else if (oldScale.startsWith("K")) { oldMultiplier = 1000; oldBase = 1; }
+            else if (oldScale.startsWith("m")) { oldMultiplier = 1024 * 1024; oldBase = 1; }
+            else if (oldScale.startsWith("M")) { oldMultiplier = 1000 * 1000; oldBase = 1; }
+            else if (oldScale.startsWith("g")) { oldMultiplier = 1024 * 1024 * 1024; oldBase = 1; }
+            else if (oldScale.startsWith("G")) { oldMultiplier = 1000 * 1000 * 1000; oldBase = 1; }
+            else if (oldScale.startsWith("b")) { oldMultiplier = 1; oldBase = 1; }
+            else if (oldScale.startsWith("B")) { oldMultiplier = 1; oldBase = 8; }
+            else return FAILED;
+        }
+        else { oldMultiplier = 1; oldBase = 1; }
+
+        if (newScale.length() == 2) {
+            if (newScale.startsWith("k")) newMultiplier = 1024;
+            else if (newScale.startsWith("K")) newMultiplier = 1000;
+            else if (newScale.startsWith("m")) newMultiplier = 1024 * 1024;
+            else if (newScale.startsWith("M")) newMultiplier = 1000 * 1000;
+            else if (newScale.startsWith("g")) newMultiplier = 1024 * 1024 * 1024;
+            else if (newScale.startsWith("G")) newMultiplier = 1000 * 1000 * 1000;
+            else return FAILED;
+
+            if (newScale.endsWith("b")) newBase = 1;
+            else if (newScale.endsWith("B")) newBase = 8;
+            else return FAILED;
+        }
+        else if (newScale.length() == 1) {
+            if (newScale.startsWith("k")) { newMultiplier = 1024; newBase = 1; }
+            else if (newScale.startsWith("K")) { newMultiplier = 1000; newBase = 1; }
+            else if (newScale.startsWith("m")) { newMultiplier = 1024 * 1024; newBase = 1; }
+            else if (newScale.startsWith("M")) { newMultiplier = 1000 * 1000; newBase = 1; }
+            else if (newScale.startsWith("g")) { newMultiplier = 1024 * 1024 * 1024; newBase = 1; }
+            else if (newScale.startsWith("G")) { newMultiplier = 1000 * 1000 * 1000; newBase = 1; }
+            else if (newScale.startsWith("b")) { newMultiplier = 1; newBase = 1; }
+            else if (newScale.startsWith("B")) { newMultiplier = 1; newBase = 8; }
+            else return FAILED;
+        }
+        else { newMultiplier = 1; newBase = 1; }
+
+        return value * oldBase * oldMultiplier / newBase / newMultiplier;
+    }
+}

src/com/android/settings/xd/extrainfo/MainActivity.java
@@ -1,0 +1,140 @@
+/*
+ * Copyright (C) 2014 The Dirty Unicorns project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.xd.extrainfo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public class MainActivity extends Activity {
+    private LinearLayout mLayout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.extrainfo_layout);
+        mLayout = (LinearLayout) findViewById(R.id.mainLinearLayout);
+        loadInfo();
+    }
+
+    private void addItem(String name, String value) { addItem(name, value, false); }
+    private void addItem(String name, String value, boolean ucWords) {
+        if (value != null && value.length() > 0) {
+            mLayout.addView(new KeyTextView(this, name));
+            if (ucWords) value = ExtraInfoLib.ucWords(value);
+            mLayout.addView(new ValueTextView(this, value));
+        }
+    }
+
+    private static class SeparatorTextView extends TextView {
+        public SeparatorTextView(Context context) {
+            super(context);
+            setBackgroundColor(0xFFFFFFFF);
+        }
+    }
+
+   private static class CategoryTextView extends TextView {
+        public CategoryTextView(Context context, String text) {
+            super(context);
+            int indent = ExtraInfoLib.dpToPx(context, 5);
+            int pad = ExtraInfoLib.dpToPx(context, 2);
+            setTextAppearance(context, android.R.style.TextAppearance_Medium);
+            setBackgroundColor(0xFF1D1D1D);
+            setTextColor(0xFFFFFFFF);
+            setGravity(Gravity.CENTER_VERTICAL);
+            setPadding(indent, pad, pad, pad);
+            setText(text);
+        }
+    }
+
+    private static class KeyTextView extends TextView {
+        public KeyTextView(Context context, String text) {
+            super(context);
+            int indent = ExtraInfoLib.dpToPx(context, 5);
+            int pad = ExtraInfoLib.dpToPx(context, 2);
+            setTextAppearance(context, android.R.style.TextAppearance_Small);
+            setPadding(indent, pad, pad, pad);
+            setText(text);
+        }
+    }
+
+    private static class ValueTextView extends TextView {
+        public ValueTextView(Context context, String text) {
+            super(context);
+            int indent = ExtraInfoLib.dpToPx(context, 20);
+            int pad = ExtraInfoLib.dpToPx(context, 2);
+            setTextColor(Color.WHITE);
+            setPadding(indent, pad, pad, pad);
+            setText(text);
+        }
+    }
+
+    private void loadInfo() {
+        ExtraInfo di = new ExtraInfo(this);
+
+        addCategory("BASIC INFO");
+
+        addItem("Manufacturer", di.getBuildManufacturer(), true);
+        addItem("Hardware type", di.getPropHardware());
+        addItem("System partition", di.getSystemSize() + " Bytes");
+        addItem("Data partition", di.getDataSize() + " Bytes");
+        addItem("RAM", di.getProcMemTotal());
+        addItem("Bootloader", di.getPropBootloader());
+
+        addCategory("CPU INFO");
+
+        addItem("Chipset / Processor", di.getProcCpuDescription());
+        addItem("Features", di.getProcCpuFeatures());
+        addItem("BogoMIPS", di.getProcCpuBogoMips());
+        addItem("ABI", di.getBuildCpuAbi());
+        addItem("ABI2", di.getPropCpuAbi2());
+        addItem("Architecture", di.getProcCpuArchitecture());
+        addItem("Implementer", di.getProcCpuImplementer());
+        addItem("Variant", di.getProcCpuVariant());
+        addItem("Part", di.getProcCpuPart());
+        addItem("Revision", di.getProcCpuRevision());
+
+        addCategory("DISPLAY INFO");
+
+        addItem("Diagonal", di.getDisplayDiagonalInches() + " Inches");
+        addItem("Width", di.getDisplayWidthInches() + " Inches");
+        addItem("Height", di.getDisplayHeightInches() + " Inches");
+        addItem("Width", di.getDisplayWidth() + " Pixels");
+        addItem("Height", di.getDisplayHeight() + " Pixels");
+        addItem("Density", di.getDisplayDensity());
+        addItem("DOTS PER INCH", di.getDisplayDpi());
+        addItem("Actual DPI X", di.getDisplayDpiX());
+        addItem("Actual DPI Y", di.getDisplayDpiY());
+        addItem("Refresh Rate", di.getDisplayRefreshRate());
+
+        }
+
+    @SuppressWarnings("deprecation")
+        private void addCategory(String name) {
+        mLayout.addView(new CategoryTextView(this, name),
+                        new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+        mLayout.addView(new SeparatorTextView(this),
+                        new LayoutParams(LayoutParams.FILL_PARENT, ExtraInfoLib.dpToPx(this, 1)));
+    }
+}

UltimateROM/android_packages_apps_Settings

src/com/android/settings/benzo/extrainfo/ExtraInfoLib.java
@@ -1,0 +1,156 @@
+/*
+ * Copyright (C) 2014 The Dirty Unicorns project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.benzo.extrainfo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import com.android.settings.R;
+
+public final class ExtraInfoLib {
+        private ExtraInfoLib() {
+                throw new AssertionError();
+        }
+
+        public static int dpToPx(Context context, int dp) {
+            float scale = context.getResources().getDisplayMetrics().density;
+            return (int) (dp * scale + 0.5f);
+        }
+
+    public static String ucFirst(String str) {
+        if (str == null) throw new NullPointerException("null");
+        if (str.length() == 0) return str;
+        char[] chars = str.toCharArray();
+        chars[0] = Character.toUpperCase(chars[0]);
+        return new String(chars);
+    }
+
+    public static String ucWords(String str) {
+        if (str == null) throw new NullPointerException("null");
+        if (str.length() == 0) return str;
+        final StringBuilder result = new StringBuilder(str.length());
+        String[] words = str.split("\\s");
+        for(int i = 0; i < words.length; ++i) {
+            if(i>0) result.append(" ");
+            result.append(ucFirst(words[i]));
+        }
+        return result.toString();
+    }
+
+    public static List<String> shellExec(String command)
+                throws IOException, SecurityException {
+        List<String> list = new ArrayList<String>();
+        BufferedReader stdInput = null;
+        try {
+            Process p = Runtime.getRuntime().exec(command);
+            stdInput = new BufferedReader(new
+                InputStreamReader(p.getInputStream()));
+            String s;
+            while ((s = stdInput.readLine()) != null) list.add(s);
+        } catch (IOException e) {
+                stdInput.close();
+                throw e;
+        }
+        catch (SecurityException e) {
+                stdInput.close();
+                throw e;
+        }
+        stdInput.close();
+
+        return list;
+    }
+
+    public static String round(double number, int decimalPlaces) {
+            return round(number, decimalPlaces, false);
+    }
+    public static String round(double number, int decimalPlaces, boolean groupDigits) {
+        NumberFormat numFormat = NumberFormat.getInstance();
+        numFormat.setMaximumFractionDigits(decimalPlaces);
+        numFormat.setGroupingUsed(groupDigits);
+        return numFormat.format(number);
+    }
+
+    public static double scaleData(double value, String oldScale, String newScale) {
+        final double FAILED = -1;
+        if (oldScale.length() > 2 || newScale.length() > 2) return FAILED;
+        if (oldScale.equals(newScale)) return value;
+
+        int oldMultiplier = 0;
+        int oldBase = 0;
+        int newMultiplier = 0;
+        int newBase = 0;
+
+        if (oldScale.length() == 2) {
+            if (oldScale.startsWith("k")) oldMultiplier = 1024;
+            else if (oldScale.startsWith("K")) oldMultiplier = 1000;
+            else if (oldScale.startsWith("m")) oldMultiplier = 1024 * 1024;
+            else if (oldScale.startsWith("M")) oldMultiplier = 1000 * 1000;
+            else if (oldScale.startsWith("g")) oldMultiplier = 1024 * 1024 * 1024;
+            else if (oldScale.startsWith("G")) oldMultiplier = 1000 * 1000 * 1000;
+            else return FAILED;
+
+            if (oldScale.endsWith("b")) oldBase = 1;
+            else if (oldScale.endsWith("B")) oldBase = 8;
+            else return FAILED;
+        }
+        else if (oldScale.length() == 1) {
+            if (oldScale.startsWith("k")) { oldMultiplier = 1024; oldBase = 1; }
+            else if (oldScale.startsWith("K")) { oldMultiplier = 1000; oldBase = 1; }
+            else if (oldScale.startsWith("m")) { oldMultiplier = 1024 * 1024; oldBase = 1; }
+            else if (oldScale.startsWith("M")) { oldMultiplier = 1000 * 1000; oldBase = 1; }
+            else if (oldScale.startsWith("g")) { oldMultiplier = 1024 * 1024 * 1024; oldBase = 1; }
+            else if (oldScale.startsWith("G")) { oldMultiplier = 1000 * 1000 * 1000; oldBase = 1; }
+            else if (oldScale.startsWith("b")) { oldMultiplier = 1; oldBase = 1; }
+            else if (oldScale.startsWith("B")) { oldMultiplier = 1; oldBase = 8; }
+            else return FAILED;
+        }
+        else { oldMultiplier = 1; oldBase = 1; }
+
+        if (newScale.length() == 2) {
+            if (newScale.startsWith("k")) newMultiplier = 1024;
+            else if (newScale.startsWith("K")) newMultiplier = 1000;
+            else if (newScale.startsWith("m")) newMultiplier = 1024 * 1024;
+            else if (newScale.startsWith("M")) newMultiplier = 1000 * 1000;
+            else if (newScale.startsWith("g")) newMultiplier = 1024 * 1024 * 1024;
+            else if (newScale.startsWith("G")) newMultiplier = 1000 * 1000 * 1000;
+            else return FAILED;
+
+            if (newScale.endsWith("b")) newBase = 1;
+            else if (newScale.endsWith("B")) newBase = 8;
+            else return FAILED;
+        }
+        else if (newScale.length() == 1) {
+            if (newScale.startsWith("k")) { newMultiplier = 1024; newBase = 1; }
+            else if (newScale.startsWith("K")) { newMultiplier = 1000; newBase = 1; }
+            else if (newScale.startsWith("m")) { newMultiplier = 1024 * 1024; newBase = 1; }
+            else if (newScale.startsWith("M")) { newMultiplier = 1000 * 1000; newBase = 1; }
+            else if (newScale.startsWith("g")) { newMultiplier = 1024 * 1024 * 1024; newBase = 1; }
+            else if (newScale.startsWith("G")) { newMultiplier = 1000 * 1000 * 1000; newBase = 1; }
+            else if (newScale.startsWith("b")) { newMultiplier = 1; newBase = 1; }
+            else if (newScale.startsWith("B")) { newMultiplier = 1; newBase = 8; }
+            else return FAILED;
+        }
+        else { newMultiplier = 1; newBase = 1; }
+
+        return value * oldBase * oldMultiplier / newBase / newMultiplier;
+    }
+}

src/com/android/settings/benzo/extrainfo/MainActivity.java
@@ -1,0 +1,140 @@
+/*
+ * Copyright (C) 2014 The Dirty Unicorns project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.benzo.extrainfo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public class MainActivity extends Activity {
+    private LinearLayout mLayout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.extrainfo_layout);
+        mLayout = (LinearLayout) findViewById(R.id.mainLinearLayout);
+        loadInfo();
+    }
+
+    private void addItem(String name, String value) { addItem(name, value, false); }
+    private void addItem(String name, String value, boolean ucWords) {
+        if (value != null && value.length() > 0) {
+            mLayout.addView(new KeyTextView(this, name));
+            if (ucWords) value = ExtraInfoLib.ucWords(value);
+            mLayout.addView(new ValueTextView(this, value));
+        }
+    }
+
+    private static class SeparatorTextView extends TextView {
+        public SeparatorTextView(Context context) {
+            super(context);
+            setBackgroundColor(0xFFFFFFFF);
+        }
+    }
+
+   private static class CategoryTextView extends TextView {
+        public CategoryTextView(Context context, String text) {
+            super(context);
+            int indent = ExtraInfoLib.dpToPx(context, 5);
+            int pad = ExtraInfoLib.dpToPx(context, 2);
+            setTextAppearance(context, android.R.style.TextAppearance_Medium);
+            setBackgroundColor(0xFF1D1D1D);
+            setTextColor(0xFFFFFFFF);
+            setGravity(Gravity.CENTER_VERTICAL);
+            setPadding(indent, pad, pad, pad);
+            setText(text);
+        }
+    }
+
+    private static class KeyTextView extends TextView {
+        public KeyTextView(Context context, String text) {
+            super(context);
+            int indent = ExtraInfoLib.dpToPx(context, 5);
+            int pad = ExtraInfoLib.dpToPx(context, 2);
+            setTextAppearance(context, android.R.style.TextAppearance_Small);
+            setPadding(indent, pad, pad, pad);
+            setText(text);
+        }
+    }
+
+    private static class ValueTextView extends TextView {
+        public ValueTextView(Context context, String text) {
+            super(context);
+            int indent = ExtraInfoLib.dpToPx(context, 20);
+            int pad = ExtraInfoLib.dpToPx(context, 2);
+            setTextColor(Color.WHITE);
+            setPadding(indent, pad, pad, pad);
+            setText(text);
+        }
+    }
+
+    private void loadInfo() {
+        ExtraInfo di = new ExtraInfo(this);
+
+        addCategory("BASIC INFO");
+
+        addItem("Manufacturer", di.getBuildManufacturer(), true);
+        addItem("Hardware type", di.getPropHardware());
+        addItem("System partition", di.getSystemSize() + " Bytes");
+        addItem("Data partition", di.getDataSize() + " Bytes");
+        addItem("RAM", di.getProcMemTotal());
+        addItem("Bootloader", di.getPropBootloader());
+
+        addCategory("CPU INFO");
+
+        addItem("Chipset / Processor", di.getProcCpuDescription());
+        addItem("Features", di.getProcCpuFeatures());
+        addItem("BogoMIPS", di.getProcCpuBogoMips());
+        addItem("ABI", di.getBuildCpuAbi());
+        addItem("ABI2", di.getPropCpuAbi2());
+        addItem("Architecture", di.getProcCpuArchitecture());
+        addItem("Implementer", di.getProcCpuImplementer());
+        addItem("Variant", di.getProcCpuVariant());
+        addItem("Part", di.getProcCpuPart());
+        addItem("Revision", di.getProcCpuRevision());
+
+        addCategory("DISPLAY INFO");
+
+        addItem("Diagonal", di.getDisplayDiagonalInches() + " Inches");
+        addItem("Width", di.getDisplayWidthInches() + " Inches");
+        addItem("Height", di.getDisplayHeightInches() + " Inches");
+        addItem("Width", di.getDisplayWidth() + " Pixels");
+        addItem("Height", di.getDisplayHeight() + " Pixels");
+        addItem("Density", di.getDisplayDensity());
+        addItem("DOTS PER INCH", di.getDisplayDpi());
+        addItem("Actual DPI X", di.getDisplayDpiX());
+        addItem("Actual DPI Y", di.getDisplayDpiY());
+        addItem("Refresh Rate", di.getDisplayRefreshRate());
+
+        }
+
+    @SuppressWarnings("deprecation")
+        private void addCategory(String name) {
+        mLayout.addView(new CategoryTextView(this, name),
+                        new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+        mLayout.addView(new SeparatorTextView(this),
+                        new LayoutParams(LayoutParams.FILL_PARENT, ExtraInfoLib.dpToPx(this, 1)));
+    }
+}