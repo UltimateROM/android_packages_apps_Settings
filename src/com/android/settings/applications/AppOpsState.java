65fd0d9ce9bdc76ddcebdbe5d6cf358a935b19e0
Shilin Victor <chrono.monochrome@gmail.com>
2016-05-19 23:58:30 +0700

    Revert "[2/2] Settings: Battery and Notification Lights"
    This reverts commit 2ae50b40daa729d217ea577b9890a869247989ab.
    
    Change-Id: Ic7364f0601a5e7fb95f7b84f6f71af00e6fbe723

--- src/com/android/settings/applications/AppOpsState.java
+++ src/com/android/settings/applications/AppOpsState.java
@@ -578,11 +578,6 @@ public class AppOpsState {
                                     appInfo.packageName, appInfo.applicationInfo.uid, dummyOps);
 
                         }
-                        AppOpsManager.OpEntry opEntry = new AppOpsManager.OpEntry(
-                                permOps.get(k), AppOpsManager.MODE_ALLOWED, 0, 0, 0);
-                        dummyOps.add(opEntry);
-                        addOp(entries, pkgOps, appEntry, opEntry, packageName == null,
-                                packageName == null ? 0 : opToOrder[opEntry.getOp()]);
                     }
                 }
             }
